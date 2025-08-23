f1cad490a9c806f5189b73b8eca2e53b74e749e7
João Paulo <jp.sport17@gmail.com>
2025-08-23 12:55:06 -0300

    cleanup: Removido código não utilizado na documentação produtos

--- src/main/java/com/jp/produtos/web/controller/docs/ProdutoDocs.java
+++ src/main/java/com/jp/produtos/web/controller/docs/ProdutoDocs.java
@@ -7,12 +7,10 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Produtos", description = "Gerenciamento de produtos")
 public interface ProdutoDocs {
