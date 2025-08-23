2ed376f5be3e12ce09bd0954e8ce33561e27cc84
João Paulo <jp.sport17@gmail.com>
2025-08-23 03:02:18 -0300

    feat: Atualizada a configuração do AuthController para produzir JSON e aprimorada a documentação da API para o endpoint de geração de token

--- src/main/java/com/jp/produtos/web/controller/docs/AuthDocs.java
+++ src/main/java/com/jp/produtos/web/controller/docs/AuthDocs.java
@@ -2,6 +2,8 @@ package com.jp.produtos.web.controller.docs;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 public interface AuthDocs {
 
     @Operation(summary = "Gerar token de autenticação", tags = {"Autenticação"})
-    @ApiResponse(responseCode = "200", description = "Token gerado com sucesso!")
+    @ApiResponse(responseCode = "200", description = "Token gerado com sucesso!",
+            content = @Content(schema = @Schema(implementation = ObjectNode.class)))
     ResponseEntity<ObjectNode> token();
 }
