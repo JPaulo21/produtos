e7956c1960af66a1f1f57fead30a5a20b02e1032
João Paulo <jp.sport17@gmail.com>
2025-08-23 02:59:12 -0300

    feat: Adicionado configuração do Swagger para documentação da API com suporte a autenticação JWT

---
+++ src/main/java/com/jp/produtos/config/docs/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.jp.produtos.config.docs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Produto API"
+                , description = "api to obtain data from football clubs"
+                , version = "1.0"
+                , contact = @Contact(name = "João Paulo", email = "jp.sport17@gmail.com")
+                , license = @License(
+
+        )
+                , termsOfService = "Terms of service"
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
+)
+public class SwaggerConfig {
+}
