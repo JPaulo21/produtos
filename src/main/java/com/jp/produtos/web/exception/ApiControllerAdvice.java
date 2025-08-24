b68f51388dc9f12c11cf7a83f2945071a41e8424
João Paulo <jp.sport17@gmail.com>
2025-08-24 12:31:20 -0300

    feat: Atualizado manipuladores de exceção para retornar ResponseEntity com mensagens de erro

--- src/main/java/com/jp/produtos/web/exception/ApiControllerAdvice.java
+++ src/main/java/com/jp/produtos/web/exception/ApiControllerAdvice.java
@@ -3,6 +3,7 @@ package com.jp.produtos.web.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,42 +24,40 @@ public class ApiControllerAdvice {
     @ResponseBody
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ErrorResponse handleException(HttpServletRequest req, Exception ex) {
+    public ResponseEntity<ErrorResponse> handleException(HttpServletRequest req, Exception ex) {
         logger.error("Exception: {}", ex.getMessage());
-        return new ErrorResponse(LocalDateTime.now(),
+        return ResponseEntity.internalServerError().body(new ErrorResponse(LocalDateTime.now(),
                 INTERNAL_SERVER_ERROR.value(),
                 INTERNAL_SERVER_ERROR.getReasonPhrase(),
                 req.getMethod(),
                 req.getRequestURI(),
-                ex.getMessage());
+                ex.getMessage()));
     }
 
     @ResponseBody
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(RuntimeException.class)
-    public ErrorResponse handleRuntimeException(HttpServletRequest req, RuntimeException ex) {
+    public ResponseEntity<ErrorResponse> handleRuntimeException(HttpServletRequest req, RuntimeException ex) {
         logger.error("RuntimeException: {}", ex.getMessage());
-        return new ErrorResponse(LocalDateTime.now(),
+        return ResponseEntity.badRequest().body(new ErrorResponse(LocalDateTime.now(),
                 BAD_REQUEST.value(),
                 BAD_REQUEST.getReasonPhrase(),
                 req.getMethod(),
                 req.getRequestURI(),
-                ex.getMessage());
+                ex.getMessage()));
     }
 
-    @ResponseBody
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse handleBindingResultException(HttpServletRequest req, MethodArgumentNotValidException e, BindingResult result) {
+    public ResponseEntity<ErrorResponse> handleBindingResultException(HttpServletRequest req, MethodArgumentNotValidException e, BindingResult result) {
         logger.info("{}", e.getMessage());
         String field = result.getFieldError().getField().toString();
         String message = result.getFieldError().getDefaultMessage();
         logger.error("Exception bindingResult: {}", field + " " + message);
-        return new ErrorResponse(LocalDateTime.now(),
+        return ResponseEntity.badRequest().body(new ErrorResponse(LocalDateTime.now(),
                 BAD_REQUEST.value(),
                 BAD_REQUEST.getReasonPhrase(),
                 req.getMethod(),
                 req.getRequestURI(),
-                "field['" + field + "']: " + message);
+                "field['" + field + "']: " + message));
     }
 }
